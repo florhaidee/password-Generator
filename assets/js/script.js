ef5cfd47ba3bc10d5150ce9de68647f5f2972188
florhaidee <63246286+florhaidee@users.noreply.github.com>
2020-05-17 11:19:39 -0600

    object created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-// Assignment code here
+var getLength= function() {
+  var length = window.prompt('Please select the length of your password, choose a number between 8 and no more than 128:');
+  length = parseInt(length);
+  console.log(length);
+  return length
+}
 
+//Object with the criteria needed to create the password
+var passwordInfo = {
+  length: getLength(),
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  numeric: "0123456789",
+  characters: '!@#$%^&*()_+~`|}{[]\:;?><,./-=',
+  pwd: '',
+}
+console.log(passwordInfo);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
